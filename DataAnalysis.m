47a40716c3c2c2875783e3771423cd98c86a0519
Katja <katjakorte@gmx.net>
2023-06-04 15:37:44 +0200

    Updated Introduction: What is cheerleading?

---
+++ DataAnalysis.m
@@ -0,0 +1,11 @@
+%% Master Thesis: Data Analysis
+% Biomechanical analysis of a toss to hands in cheer sport: qualitative and
+% quantitative characteristics
+
+% by Katja Korte
+% katja.korte@sport.uni-giessen.de
+
+%% LOAD VARIABLES
+
+
+%% 
