8badcfc1b0bc57ea0935e69725f0e1da9abbe504
Katja <katjakorte@gmx.net>
2023-08-12 14:47:33 +0200

    Read Data, store in Structs by marker name

--- DataAnalysis.m
+++ DataAnalysis.m
@@ -5,7 +5,22 @@
 % by Katja Korte
 % katja.korte@sport.uni-giessen.de
 
+clear
+clc
+
 %% LOAD VARIABLES
+cd 'C:\Users\katja\OneDrive - Justus-Liebig-Universität Gießen\Uni\M. Sc. Human Movement Analytics\MA-HMA-14 MASTER THESIS\TossThesis\DATA'
+files = dir('*.mat');
+for i=1:length(files)
+    load(files(i).name);
+end
+
+ASIS_F = zeros([1113, 4]);
+for ctr = 1:4
+    ASIS_F(1:length(eval(strcat("NL0", string(ctr),".ASIS_F"))), ctr) = eval(strcat("NL0", string(ctr),".ASIS_F"));
+end
+
+plot(ASIS_F)
 
 
 %% 
